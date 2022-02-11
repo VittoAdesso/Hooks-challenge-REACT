2c8dd64d5f4256cc033342072f01622f9508666a
Vittorio Adesso <adessovittorio8@gmail.com>
2022-02-11 23:27:18 +0100

    Modf

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-
 import './App.scss';
+import RegisterTime from './components/RegisterTime/RegisterTime';
+import SingStar from './components/SingStar/SingStar';
+import BeerCounterTime from './components/BeerTime/BeerCounterTime';
 
 function App() {
   return (
     <div className="App">
-      
+
+      <BeerCounterTime/>
+
+      <SingStar/>
+
+      <RegisterTime/>
+
     </div>
   );
 }
