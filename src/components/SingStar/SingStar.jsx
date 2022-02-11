467961d73c84dc81ebc1415a270f28788d39f38c
Vittorio Adesso <adessovittorio8@gmail.com>
2022-02-11 22:52:24 +0100

    Input component

--- src/components/SingStar/SingStar.jsx
+++ src/components/SingStar/SingStar.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react'
+
+import './styles.scss'
+
 
 const SingStar = () => {
-  return <div></div>;
+
+  const [singer, setSinger] = useState(''); 
+
+  useEffect(() => {
+    return ` ${singer}`; 
+
+    }, [singer]);
+
+
+  return (
+
+  <div className='inputSinger'>
+
+    <h2>Register</h2>
+
+    <input value={singer} onChange={(event) => setSinger(event.target.value)} />
+
+    <p> { singer ? 'WELCOME' : 'Please type some name' } </p>
+
+     <h3> {singer} </h3> 
+
+
+
+  </div>
+  );
 };
 
 export default SingStar;
